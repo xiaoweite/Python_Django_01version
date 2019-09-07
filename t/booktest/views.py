0dee7aedfdd1f096d76de14a93e5f18058a7a36f
Lanson <1096329747@qq.com>
2019-09-07 16:18:00 +0800

    Django --- 数据文件和URL路由配置

--- t/booktest/views.py
+++ t/booktest/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import *
+def index(request):
+    return  HttpResponse('hello world')
 
-# Create your views here.
