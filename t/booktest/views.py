cb650269c977ec3097b5a4edfc646c1e921e5b1b
Lanson <1096329747@qq.com>
2019-09-07 17:32:18 +0800

    Django --- 查询数据并且在模板循环显示

--- t/booktest/views.py
+++ t/booktest/views.py
@@ -1,10 +1,14 @@
+#encoding=utf-8
 from django.shortcuts import render
 from django.http import *
+from .models import *
 # from django.template import RequestContext,loader
 def index(request):
     # temp=loader.get_template('booktest/index.html')
     # return  HttpResponse(temp.render())
-    context = {'title':'43423'}
+    # context = {'title':'43423'}
+    bookList=BookInfo.objects.all()
+    context = {'list':bookList}
     return render(request,'booktest/index.html',context)
 
 
